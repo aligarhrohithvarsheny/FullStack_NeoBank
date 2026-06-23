d37ae6c25814b299536a4d3303cb0d753bc70da1
aligarhrohithvarsheny <sairohith669@gmail.com>
2026-06-23 19:19:38 +0530

    your message

--- angularapp/src/environment/environment.prod.ts
+++ angularapp/src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // Production backend endpoint (Render deployment)
-  apiBaseUrl: 'http://localhost:8080'
+  // Production backend (Render). Overridden at build time by replace-env.js when BACKEND_API_URL is set.
+  apiBaseUrl: 'https://fullstack-neobank.onrender.com'
 };
