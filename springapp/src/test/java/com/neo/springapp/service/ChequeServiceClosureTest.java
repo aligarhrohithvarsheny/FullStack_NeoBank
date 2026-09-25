618034b6be27f929c8cce1def7090940458c2fa4
aligarhrohithvarsheny <sairohith669@gmail.com>
2026-09-25 20:14:55 +0530

    your message

--- springapp/src/test/java/com/neo/springapp/service/ChequeServiceClosureTest.java
+++ springapp/src/test/java/com/neo/springapp/service/ChequeServiceClosureTest.java
@@ -48,6 +48,8 @@ class ChequeServiceClosureTest {
                 null,
                 null,
                 null,
+                null,
+                null,
                 null
         );
 
